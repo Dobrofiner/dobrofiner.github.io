5625b093a3d7165ca9ef08777840599f241aeb1d
Dobroposter <smit.1t@yandex.ru>
2024-05-21 20:31:42 +0700

    Update

--- start.bat
+++
@@ -1,3 +0,0 @@
-@start start_server_PYTHON.bat
-@ngrok http --domain=obviously-flexible-reindeer.ngrok-free.app 8000
-@pause
--- start_server_PYTHON.bat
+++
@@ -1,3 +0,0 @@
-@TITLE Python Server
-@START http://localhost:8000/
-@python -m http.server
